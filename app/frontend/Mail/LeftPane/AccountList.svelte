TenforwardAB/hugin

commit 1a441ea9979f108458c068402cc8a076e80bc05e

Mail: Move popup: Move to other account

app/frontend/Mail/LeftPane/AccountList.svelte
@@ -1,4 +1,4 @@
-<vbox class="account-list">
+<vbox class="account-list" flex>
   <FastList items={accounts} bind:selectedItem={selectedAccount} columns="1fr">
     <svelte:fragment slot="header">
       <hbox class="header">

app/frontend/Mail/LeftPane/FolderList.svelte
@@ -2,7 +2,9 @@
   <FastTree items={foldersSorted} bind:selectedItem={selectedFolder} bind:selectedItems={selectedFolders}
     columns="auto">
     <svelte:fragment slot="header">
-      <hbox class="header">{$t`Folders`}</hbox>
+      <slot name="header">
+        <hbox class="header">{$t`Folders`}</hbox>
+      </slot>
     </svelte:fragment>
     <TreeItemLine slot="row" let:item={folder} item={folder}>
       <FolderLine {folder} slot="row">

app/frontend/Mail/Message/MessageMovePopup.svelte
@@ -27,19 +27,25 @@
       icon={CloseIcon}
       />
   </hbox>
-  <vbox class="tags">
-    <hbox class="header">{$t`Tags`}</hbox>
-    <!-- TODO make work with multiple selected messages -->
-    <TagSelector
-      tags={availableTags}
-      selectedTags={messages.first.tags}
-      message={messages.first}
-      on:select={onClose}
-      on:unselect={onClose}
-      />
-  </vbox>
+  {#if !showAccounts}
+    <vbox class="tags">
+      <hbox class="header">{$t`Tags`}</hbox>
+      <!-- TODO make work with multiple selected messages -->
+      <TagSelector
+        tags={availableTags}
+        selectedTags={messages.first.tags}
+        message={messages.first}
+        on:select={onClose}
+        on:unselect={onClose}
+        />
+    </vbox>
+  {/if}
+  {#if showAccounts}
+    <vbox class="accounts">
+      <AccountList accounts={appGlobal.emailAccounts} bind:selectedAccount />
+    </vbox>
+  {/if}
   <vbox class="folders">
-    <AccountList accounts={appGlobal.emailAccounts} bind:selectedAccount />
     <FolderList folders={selectedAccount.rootFolders} bind:selectedFolder bind:selectedFolders>
       <svelte:fragment slot="buttons" let:folder>
         {#if folder != sourceFolder}
@@ -57,6 +63,20 @@
             icon={MoveIcon}
             />
         {/if}
+      </svelte:fragment>
+      <svelte:fragment slot="header">
+        <hbox class="folders-header" flex>
+          {$t`Folder`}
+          <hbox flex />
+          <Button
+            label={$t`Move to other mail account`}
+            icon={AccountsIcon}
+            iconOnly
+            plain
+            selected={showAccounts}
+            onClick={() => showAccounts = !showAccounts}
+           />
+        </hbox>
       </svelte:fragment>
     </FolderList>
   </vbox>
@@ -77,6 +97,7 @@
   import ArchiveIcon from "lucide-svelte/icons/archive";
   import MoveIcon from "lucide-svelte/icons/folder-input";
   import CopyIcon from "lucide-svelte/icons/mails";
+  import AccountsIcon from "lucide-svelte/icons/share";
   import CloseIcon from "lucide-svelte/icons/x";
   import { ArrayColl, Collection } from "svelte-collections";
   import { t } from "../../../l10n/l10n";
@@ -89,6 +110,7 @@
   let selectedFolder = sourceFolder;
   let selectedFolders = new ArrayColl<Folder>();
   let selectedAccount = sourceFolder.account;
+  let showAccounts = false;
 
   function onClose() {
     dispatch("close");
@@ -151,8 +173,11 @@
     margin: 10px;
     max-width: 300px;
   }
+  .accounts {
+    height: 10em;
+  }
   .folders {
-    height: 20em;
+    height: 22em;
   }
   .buttons {
     border-top: 1px solid var(--border);

commit b05fd4c0416d0fc0cd884463c396e7bff5216ef1

Mail: Account list in left pane should be minimal size - regression from 1a441ea9

app/frontend/Mail/LeftPane/AccountList.svelte
@@ -1,4 +1,4 @@
-<vbox class="account-list" flex>
+<vbox class="account-list">
   <FastList items={accounts} bind:selectedItem={selectedAccount} columns="1fr">
     <svelte:fragment slot="header">
       <hbox class="header">
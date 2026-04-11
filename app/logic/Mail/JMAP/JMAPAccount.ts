TenforwardAB/hugin

commit 328a7954e3be1f26de47c241896c48b27931329f

Account: Generic logout() and disconnect()

app/logic/Mail/JMAP/JMAPAccount.ts
@@ -407,12 +407,10 @@ export class JMAPAccount extends MailAccount {
     }
   }
 
-  async logout(): Promise<void> {
+  async disconnect(): Promise<void> {
     this.stopPolling();
     this.session = null;
-    if (this.oAuth2) {
-      await this.oAuth2.logout();
-    }
+    await super.logout();
   }
 
   async send(email: EMail): Promise<void> {

app/logic/Abstract/Account.ts
@@ -110,6 +110,8 @@ export class Account extends Observable {
   }
 
   async logout(): Promise<void> {
+    await this.disconnect();
+    await this.oAuth2?.logout();
   }
 
   /**

commit 1b1e5b8f9e8230202dec3ec96bd2936b606c93cf

JMAP: Fix logout - regressed by 328a7954e

app/logic/Mail/JMAP/JMAPAccount.ts
@@ -410,7 +410,6 @@ export class JMAPAccount extends MailAccount {
   async disconnect(): Promise<void> {
     this.stopPolling();
     this.session = null;
-    await super.logout();
   }
 
   async send(email: EMail): Promise<void> {